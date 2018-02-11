38aaa29feb148ee44072fe1cbd6b9c2f4370a558
CodoBomber <fantasy.reactor@gmail.com>
2018-02-12 02:20:56 +0700

    Initial commit

---
+++ src/generators/RandomNumberGenerator.java
@@ -0,0 +1,32 @@
+package generators;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomNumberGenerator {
+
+    private final ThreadLocalRandom random = ThreadLocalRandom.current();
+
+    public RandomNumberGenerator() {
+        generate();
+    }
+
+    private void generate() {
+        double ksi, x, y, f;
+        for (int i = 0; i < 10000; i++) {
+            ksi = Math.random();
+            if (ksi <= 4d/7d) {
+                do {
+                    x = Math.random();
+                    y = random.nextDouble(0, 12 / 7);
+                    f = 12 / 7 * Math.pow(x - 1, 2);
+                    if (y < f) {
+                        System.out.println(x);
+                    }
+                } while (y > f);
+            } else  {
+                System.out.println(4d * ksi - (11d / 7d));
+            }
+        }
+    }
+
+}
