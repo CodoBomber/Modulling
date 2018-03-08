df83fcafdb8c05f62fe55c8d2385c347edf3559c
chernov <a.chernov@e2e4online.ru>
2018-03-08 20:42:39 +0700

    オペレーション研究ラボ完成です。

--- src/operation_reserching/SimplexMethod.java
+++ src/operation_reserching/SimplexMethod.java
@@ -1,7 +1,5 @@
 package operation_reserching;
 
-import java.util.Arrays;
-
 public class SimplexMethod {
 
     private double[][] A;
@@ -13,8 +11,7 @@ public class SimplexMethod {
     }
 
     public void findOptimalSolutions() {
-        while (isNegativeNumberCondition(A[0])) {
-            findLeadColumn(A[0]);
+        for (int min = findLeadColumn(A[0]); min != -1; min = findLeadColumn(A[0])) {
             findLeadRow();
             calculateLeadRow();
             for (int i = 0; i < A.length; i++) {
@@ -28,59 +25,72 @@ public class SimplexMethod {
 
     private void printOptimalSolutions() {
         System.out.println("#########OPTIMAL SOLUTION:#########");
-        System.out.println(A[0][A.length - 1]);
-        System.out.println(A[1][A.length - 1]);
-        System.out.println(A[2][A.length - 1]);
+        System.out.println(A[1][A[1].length - 1]);
+        System.out.println(A[2][A[2].length - 1]);
+        System.out.println(A[0][A[0].length - 1]);
         System.out.println("###################################");
     }
 
     private void calculateRow(int rowIndex) {
+        double leadElement = A[rowIndex][leadColumn];
         for (int i = 0; i < A[rowIndex].length; i++) {
-            A[rowIndex][i] -= A[rowIndex][leadColumn] * A[leadRow][i];
+            A[rowIndex][i] -= leadElement * A[leadRow][i];
         }
     }
 
     private void calculateLeadRow() {
+        double leadElement = A[leadRow][leadColumn];
         for (int i = 0; i < A[leadRow].length; i++) {
-            A[leadRow][i] /= A[leadRow][leadColumn];
+            A[leadRow][i] /= leadElement;
         }
     }
 
     private void findLeadRow() {
-        double min = A[0][A[0].length - 1] / A[0][leadColumn];
-        int minIndex;
+        /*double min;
         double local_min;
-        for (minIndex = 1; minIndex < A.length; minIndex++) {
+        for (int minIndex = 1; minIndex < A.length; minIndex++) {
+            min = A[minIndex][A[minIndex].length - 1] / A[minIndex][leadColumn];
             local_min = A[minIndex][A[minIndex].length - 1] / A[minIndex][leadColumn];
-            min = local_min >= 0 && local_min < min ? local_min : min;
+            if (local_min >= 0 && local_min < min) {
+                min = local_min;
+                leadRow = minIndex;
+            }
+        }*/
+        double min = A[1][A[1].length - 1] / A[1][leadColumn], localMin;
+        leadRow = 1;
+        for (int i = 2; i < A.length; i++) {
+            localMin = A[i][A[i].length - 1] / A[i][leadColumn];
+            if (localMin > 0 && localMin < min) {
+                min = localMin;
+                leadRow = i;
+            }
         }
-        leadRow = minIndex;
     }
 
-    private boolean isNegativeNumberCondition(double[] row) {
-        return Arrays.stream(row)
-                .boxed()
-                .anyMatch(v -> v < 0);
-    }
-
-    private void findLeadColumn(double[] row) {
+    private int findLeadColumn(double[] row) {
         int min = 0;
-        double minValue = Double.MAX_VALUE;
-        for (int i = 0; i < row.length; i++) {
-            if (minValue > row[i]) {
+        double minValue = row[0];
+        for (int i = 1; i < row.length; i++) {
+            if (row[i] < minValue) {
                 minValue = row[i];
                 min = i;
             }
         }
-        leadColumn = min;
+        if (row[min] < 0) {
+            leadColumn = min;
+            return min;
+        }
+        return -1;
     }
 
     public void prettyPrint() {
+        System.out.println("############MATRIX#PRINT###########");
         for (double[] row : A) {
             for (double column : row) {
                 System.out.print(column + " ");
             }
             System.out.println();
         }
+        System.out.println("###################################");
     }
 }
