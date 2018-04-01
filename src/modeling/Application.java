cd0a6d786514647ed835fcde972a082a343e4dbd
Kirill Semendyaev <semendyaev.96@gmail.com>
2018-04-01 23:41:27 +0700

    Only added distribution initialization

--- src/modeling/Application.java
+++ src/modeling/Application.java
@@ -1,8 +1,19 @@
 package modeling;
 
+import modeling.generators.distribution.Distribution;
+import modeling.generators.distribution.ExponentialDistribution;
+
 public class Application {
 
     public static void main(String[] args) {
+        int arrivalExpectedValue = 10;
+        int queuingExpectedValue = 1;
+
+        Distribution arrivalDistribution = new ExponentialDistribution(arrivalExpectedValue);
+        Distribution queuingDistribution = new ExponentialDistribution(queuingExpectedValue);
+
+
+
 
     }
 
